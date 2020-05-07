2f4ab54eac1d51cb0c151741a9ef75d5c3e0e0f4
enelesmai <xochitl.selene.fp@gmail.com>
2020-05-07 14:10:53 -0500

    display data

--- hello-world/src/components/HelloWorld.vue
+++ hello-world/src/components/HelloWorld.vue
@@ -1,6 +1,25 @@
 <template>
   <div class="hello container">
     <h1>{{ msg }}</h1> 
+    <h3>Users:</h3>
+     <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Id</th>
+          <th scope="col">Name</th>
+          <th scope="col">Email</th>
+          <th scope="col">City</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="user in users" v-bind:key="user.id"> 
+          <th scope="row">{{user.id}}</th>
+          <td>{{user.name}}</td>
+          <td>{{user.email}}</td>
+          <td>{{user.address.city}}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
